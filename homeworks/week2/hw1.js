2facf9966aa20162f7b81cb03d1a4014289d0f8e
tzutzu858 <chenyingtzu858@gmail.com>
2020-06-27 15:13:40 +0800

    week2 完成

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,7 @@
 function printStars(n) {
-  
+  for (i=1;i<=n;i++){
+  console.log('*') 
+  }
 }
 
 printStars(5);
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,11 @@
 function reverse(str) {
-  
+    var sum=''
+    for (i=str.length-1;i>=0;i--){
+         sum+=str[i] 
+        
+    }
+    console.log(sum)
+    
 }
 
 reverse('hello');
